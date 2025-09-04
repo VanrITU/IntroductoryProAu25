b9b702dc97298800733ab5d43939f2510d522042
VanrITU <vanritudk@gmail.com>
2025-09-04 12:38:06 +0200

    Exercises solved

--- Exercises/Week 36 - Exercises/chapter03/src/Main.java
+++ Exercises/Week 36 - Exercises/chapter03/src/Main.java
@@ -1,7 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-        Write your code below. If you are unsure as to what some of the questions mean, please ask, we are glad to help!
-        */
+        //  NumberDisplay numberDisplay = new NumberDisplay(60);
+        //        numberDisplay.setValue(0);
+        //
+        //        String valueprint = numberDisplay.getDisplayValue();
+        //        System.out.println(valueprint);
+
+        // 3.10
+
+        // numberDisplay.setValue(-20);
+
+        // it return 00 in the case of an illegal value
+
+
+        // 3.11
+
+        //it will now not include 0
+
+
+        //3.12
+
+        // one of the statements has to be true in order to be executed
+
+
+        //3.26
+
+        //modulo is better imo because the if would be longer of a calc
+
+
+        // 3.27
+        ClockDisplay CD = new ClockDisplay();
+        System.out.println(CD.getTime());
+        //it starts like this because we havent set a time
+
+
+        //3.31
+
+        //the constructor makes it possible to set a clock with a specific time instead of 00:00
+
+
+        //3.32
+
+        //because it has the update in the setTime method it is calling
+
+
+        //3.33
+
+        //p1.print(filename, true);
+        //p1.getStatus(30);
+
+
     }
 }
