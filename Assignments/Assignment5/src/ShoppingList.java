2dce56820ee8d8b25a95634c9f51e91f92077dac
VanrITU <vanritudk@gmail.com>
2025-09-25 14:23:49 +0200

    New assignment

---
+++ Assignments/Assignment5/src/ShoppingList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ShoppingList {
+    private ArrayList<String> availableItems = new ArrayList<>();
+    private ArrayList<String> recipeItems = new ArrayList<>();
+}
+
+
+
+void read() {
+
+}
+
+//void printshoppinglist
