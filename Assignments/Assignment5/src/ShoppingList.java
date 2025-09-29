cd35c3e49ddba8a1e7c611aaa9ce00b67fe5c9da
VanrITU <vanritudk@gmail.com>
2025-09-29 22:07:37 +0200

    edits

--- Assignments/Assignment5/src/ShoppingList.java
+++ Assignments/Assignment5/src/ShoppingList.java
@@ -4,12 +4,29 @@ import java.util.Scanner;
 public class ShoppingList {
     private ArrayList<String> availableItems = new ArrayList<>();
     private ArrayList<String> recipeItems = new ArrayList<>();
+
+
+
+    // constructor
+
+
+
+
+    public void read() {
+        //from user input
+        //read the available ingredients at home and one recipe or more recipes
+        // in specific format
+        // read available and store contents
+        // read recipe and store contents
+
+        // all ingredients is like this amount(whole num)-unit(g or kg)-ingredient(string
+    }
+
+    public void printShoppingList() {
+        //must start with " Shopping List:"
+        //print each line the required amount of each ingredient (g) and the name
+        //if no itmes then just title
+    }
+
+
 }
-
-
-
-void read() {
-
-}
-
-//void printshoppinglist
