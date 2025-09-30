6acb6f4b2e2497b0291d45a5c512bcd82167a4b8
VanrITU <vanritudk@gmail.com>
2025-09-30 03:14:55 +0200

    solved assignment
    lots of changes overall that are not even visible

--- Assignments/Assignment5/src/Main.java
+++ Assignments/Assignment5/src/Main.java
@@ -1,2 +1,33 @@
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        String input = """
+            AVAILABLE
+            1 kg lemon
+            200 g apple
+            500 g tomato
+            300 g salt
+            RECIPE 1
+            100 g lemon
+            500 g milk
+            1 kg rice
+            800 g tomato
+            RECIPE 2
+            1 kg apple
+            100 g tomato
+            300 g hazelnut
+            500 g almond
+            1 kg walnuts
+            """;
+
+        ByteArrayInputStream testInput = new ByteArrayInputStream(input.getBytes());
+        System.setIn(testInput);
+        ShoppingList list = new ShoppingList();
+        list.read();
+        list.printShoppingList();
+    }
+
 }
--- Assignments/Assignment5/src/ShoppingList.java
+++ Assignments/Assignment5/src/ShoppingList.java
@@ -1,32 +1,85 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class ShoppingList {
-    private ArrayList<String> availableItems = new ArrayList<>();
-    private ArrayList<String> recipeItems = new ArrayList<>();
+    private TreeMap<String, Integer> inventory = new TreeMap<>();
+    private TreeMap<String, Integer> required = new TreeMap<>();
+    private Scanner sc = new Scanner(System.in);
 
+    public ShoppingList() {
+        inventory.clear();
+        required.clear();
+    }
 
-
-    // constructor
-
+    private void addingTo(TreeMap<String, Integer> mapName, String name, int amount) {
+        if (mapName.containsKey(name)) {
+            int num = mapName.get(name);
+            mapName.put(name, num + amount);
+        } else {
+            mapName.put(name, amount);
+        }
+    }
 
 
 
     public void read() {
-        //from user input
-        //read the available ingredients at home and one recipe or more recipes
-        // in specific format
-        // read available and store contents
-        // read recipe and store contents
+        boolean isInventory = false;
+        boolean isRequired = false;
 
-        // all ingredients is like this amount(whole num)-unit(g or kg)-ingredient(string
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+
+            if (line.equalsIgnoreCase("AVAILABLE")) {
+                isInventory = true;
+                isRequired = false;
+                continue;
+            } else if (line.startsWith("RECIPE")) {
+                isInventory = false;
+                isRequired = true;
+                continue;
+            }
+
+            String[] details = line.split(" ");
+            if (details.length < 3) {
+                continue;
+            }
+
+            int amount = Integer.parseInt(details[0]);
+            String unit = details[1];
+            if (unit.equals("kg")) {
+                amount *= 1000;
+            } else if (!unit.equals("g")) { //this needs to be there unless the loop will stop at some point
+                continue;
+            }
+
+            String name = details[2];
+
+            if (isInventory) {
+                addingTo(inventory, name, amount);
+            } else if (isRequired) {
+                addingTo(required, name, amount);
+            }
+        }
+
+        for (String name : inventory.keySet()) {
+            if ((required.getOrDefault(name, 0)) <= (inventory.get(name) )) {
+                required.remove(name);
+            } else {
+                required.put(name, (required.getOrDefault(name, 0)) - (inventory.get(name) ));
+            }
+        }
     }
+
+
 
     public void printShoppingList() {
-        //must start with " Shopping List:"
-        //print each line the required amount of each ingredient (g) and the name
-        //if no itmes then just title
+        System.out.println("Shopping List:");
+        if (required.isEmpty()) {
+            return;
+        }
+
+        for (String ingredient : required.keySet()) {
+            int amount = required.get(ingredient);
+            System.out.println(amount + " g " + ingredient);
+        }
     }
-
-
-}
+    }
