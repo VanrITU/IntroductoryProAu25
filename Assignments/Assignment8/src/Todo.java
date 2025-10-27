86cba73dcf2890521ceae42b185beeb076d107aa
VanrITU <vanritudk@gmail.com>
2025-10-27 21:14:20 +0100

    redttelse

--- Assignments/Assignment8/src/Todo.java
+++ Assignments/Assignment8/src/Todo.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
 
 public class Todo {
     private ArrayList<Task> todoList = new ArrayList<Task>();
@@ -43,43 +43,105 @@ public class Todo {
         } else {
             totalTasksCom += todoList.get(index).getTime();
             todoList.remove(index);
-
         }
     }
 
 
     public void printPriority(int limit) {
         System.out.println("Filtered todo:\n--------------");
+
+        // hvis der ikk er en task med den prio skal den skrive det der
+        boolean isIn = false;
+
+
         for (Task task : todoList) {
-            if (task.getPrio()<= limit) {
-                System.out.println(task.toString());
-            } else {
+            if (limit == task.getPrio()) {
+                isIn = true;
+
+            }
+
+        }
+        for (Task task : todoList) {
+            if (isIn == false) {
                 System.out.println("No tasks with given priority");
-                break;
+
+            } else {
+                if (task.getPrio()<= limit) {
+                    System.out.println(task.toString());
+                }
             }
         }
+
+
     }
 
     public void printPrioritized() {
-        //StringBuilder sb = new StringBuilder("Prioritized todo:\n-----------------");
+        StringBuilder sb = new StringBuilder("Prioritized todo:\n-----------------\n");
+        ArrayList<Integer> prio1list = new ArrayList<Integer>();
+        ArrayList<Integer> prio2list = new ArrayList<Integer>();
+        ArrayList<Integer> prio3list = new ArrayList<Integer>();
+        ArrayList<Integer> prio4list = new ArrayList<Integer>();
 
-        /*for (Task task : todoList) {
-            if (task.getPrio() == 1){
-                if (task.getTime() == )
+        for (Task task : todoList) {
+
+            if (task.getPrio() == 1) {
+                prio1list.add(task.getTime());
+            } else if (task.getPrio() == 2) {
+                prio2list.add(task.getTime());
+            } else if (task.getPrio() == 3) {
+                prio3list.add(task.getTime());
+            } else if (task.getPrio() == 4) {
+                prio4list.add(task.getTime());
             }
-        }*/
 
-        System.out.print("Prioritized todo:\n-----------------");
-        todoList.sort(new Comparator<Task>() {
-            public int compare(Task t1, Task t2) {
-                if (t1.getPrio() != t2.getPrio()) {
-                    return Integer.compare((t1.getPrio()),(t2.getPrio()));
-                } else {
-                    return Integer.compare((t1.getTime()),(t2.getTime()));
+        }
+
+        Collections.sort(prio1list);
+        Collections.sort(prio2list);
+        Collections.sort(prio3list);
+        Collections.sort(prio4list);
+
+        for  (Integer time : prio1list) {
+            for (Task task : todoList) {
+                if (task.getTime() == time) {
+                    sb.append(task.toString());
+                    sb.append("\n");
                 }
             }
+        }
 
-        });
+        for  (Integer time : prio2list) {
+            for (Task task : todoList) {
+                if (task.getTime() == time) {
+                    sb.append(task.toString());
+                    sb.append("\n");
+                }
+            }
+        }
+
+        for  (Integer time : prio3list) {
+            for (Task task : todoList) {
+                if (task.getTime() == time) {
+                    sb.append(task.toString());
+                    sb.append("\n");
+                }
+            }
+        }
+
+        for  (Integer time : prio4list) {
+            for (Task task : todoList) {
+                if (task.getTime() == time) {
+                    sb.append(task.toString());
+                    sb.append("\n");
+                }
+            }
+        }
+        sb.delete(sb.length()-1, sb.length());
+        System.out.println(sb);
+
+
+        //System.out.print("Prioritized todo:\n-----------------");
+
 
 
 
